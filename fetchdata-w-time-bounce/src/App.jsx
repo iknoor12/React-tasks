2113907f32e14213e6a5576e142af9f484ce2ef8
iknoor12 <imiknoor@gmail.com>
2025-11-17 11:06:18 +0530

    modified theme switcher task's css

---
+++ fetchdata-w-time-bounce/src/App.jsx
@@ -0,0 +1,12 @@
+import Fetchdata from './components/Fetchdata.jsx'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Fetchdata />
+    </>
+  )
+}
+
+export default App
---
+++ fetchdata-w-time-bounce/src/components/Fetchdata.jsx
@@ -0,0 +1,7 @@
+import React, {useState, useEffect} from "react";
+
+function Fetchdata() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+}
+export default Fetchdata;
